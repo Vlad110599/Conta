2c648abf4dccf4b1ae6d800a6c8922f1a7c05561
Vlad Purcariu <vladut.purcariu@student.unitbv.ro>
2021-05-09 17:26:42 +0300

    Aplicatie Contabilitate

---
+++ AplicatieContabilitate/AplicatieContabilitate/Puncte/Punct1.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace AplicatieContabilitate.Puncte
+{
+    /// <summary>
+    /// Interaction logic for Punct1.xaml
+    /// </summary>
+    public partial class Punct1 : Window
+    {
+        public Punct1()
+        {
+            InitializeComponent();
+        }
+
+        private void CloseButton_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+    }
+}
