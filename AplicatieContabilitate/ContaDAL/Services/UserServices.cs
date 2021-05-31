5276a48e04d345ee98c375f62118b3056b8947d1
Zidaru <marian.zidaru@student.unitbv.ro>
2021-05-31 13:30:00 +0300

    Documentatie

---
+++ AplicatieContabilitate/ContaDAL/Services/UserServices.cs
@@ -0,0 +1,52 @@
+﻿using ContaDAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContaDAL.Services
+{
+    class UserServices
+    {
+        public void AddUser(User user)
+        {
+
+            using (var context = new MasterContext())
+            {
+                context.Set<User>().Add(user);
+                context.SaveChanges();
+            }
+
+        }
+
+        public List<User> GetUser()
+        {
+            using (var context = new MasterContext())
+            {
+                return context.Utilizator.ToList();
+            }
+
+        }
+
+        public void Update(User user)
+        {
+            using (var context = new MasterContext())
+            {
+                context.Entry(user).State = EntityState.Modified;
+                context.SaveChanges();
+            }
+        }
+
+        public void DeleteClient(User user)
+        {
+            using (var context = new MasterContext())
+            {
+                context.Entry(user).State = EntityState.Deleted;
+                context.SaveChanges();
+            }
+
+        }
+    }
+}
